5ca7ee15cbeaab18231f39c99fa6b83ee82247a4
Urielminpro705 <urielmata37@hotmail.com>
2023-06-16 21:45:58 -0600

    XD

---
+++ Hospital/src/App.java
@@ -0,0 +1,7 @@
+import java.util.PriorityQueue;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Hospital/src/paciente.java
@@ -0,0 +1,13 @@
+public class paciente {
+    private String nombre;
+    private String curp;
+    private int edad;
+    private String sintomas;
+    private int prioridad;
+
+    public  paciente(){}
+
+    public void calcularPrioridad(){
+        
+    }
+}
