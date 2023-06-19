a8dfca92f8b784beea3aa2c01674b5710182c52c
Uriel Mata Castellanos <urielmata37@hotmail.com>
2023-06-18 23:29:49 -0600

    Comentarios

--- Hospital/src/Proyecto.java
+++ Hospital/src/Proyecto.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 public class Proyecto {
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
+        // Se usa un comparator para establecer la logica del PriorityQueue
         Comparator<paciente> logica = new Comparator<paciente>() {
             @Override
+            // Se hace de esta manera para que se ordene de mayor a menor
             public int compare(paciente pac1, paciente pac2) {
                 if (pac1.getPrioridad().compareTo(pac2.getPrioridad()) > 0) {
                     return -1; // pac1 tiene mayor prioridad que pac2
@@ -16,35 +18,36 @@ public class Proyecto {
                 }
             }
         };
-        PriorityQueue<paciente> cola = new PriorityQueue<>(logica);
-        int opcion;
-        do{
-            opcion = menuPrincipal();
-            switch(opcion){
+        PriorityQueue<paciente> cola = new PriorityQueue<>(logica); // Se crea la cola de prioridad con objetos de tipo paciente
+        int opcion; // Se declara la variable opcion
+        do{ // Ciclo que sirve para repetir el programa hasta que se escoga la opcion 4
+            opcion = menuPrincipal(); // Se llama la funcion menuPrincipal() en la variable opcion
+            switch(opcion){ // Se hace un switch con la variable opcion
                 case 1:
-                    cola.offer(menuSecundario());
+                    cola.offer(menuSecundario()); // Se llama a la funcion menuSecundario() y se inserta en la cola
                 break;
 
                 case 2:
-                    cola.poll();
+                    cola.poll(); // Se saca un objeto de la cola
                 break;
 
                 case 3:
-                    imprimir(cola);
+                    imprimir(cola); // Se llama a la funcion imprimir()
                 break;
 
                 case 4:
-                    System.out.println("Adios");
+                    System.out.println("Adios"); // Se imprime un mensaje de despedida
                 break;
 
                 default:
-                    System.out.println("Opcion no valida");
+                    System.out.println("Opcion no valida"); // Se imprime un mensaje de error
                 break;
             }
-        }while(opcion != 4);
+        }while(opcion != 4); // El ciclo sigue hasta que la opcion sea 4
         
     }
 
+    // Funcion para imprimir el menu de opciones principal
     public static int menuPrincipal() {
         Scanner lector = new Scanner(System.in);
         int opcion;
@@ -54,28 +57,30 @@ public class Proyecto {
         System.out.println("3) Mostrar paciente");
         System.out.println("4) Salir");
         opcion = lector.nextInt();
-        return opcion;
+        return opcion; // Regresa la opcion seleccionada
     }
 
+    // Funcion para imprimir el menu de opciones secundario
     public static paciente menuSecundario() {
         Scanner lector = new Scanner(System.in);
-        paciente nuevo = new paciente();
-        System.out.println("\nIngresa el nombre");
-        nuevo.setNombre(lector.nextLine());
-        System.out.println("Ingresa la curp");
-        nuevo.setCurp(lector.nextLine());
-        System.out.println("Ingresa la edad");
-        nuevo.setEdad(lector.nextInt());
+        paciente nuevo = new paciente(); // Se crea un objeto de tipo paciente
+        System.out.println("\nIngresa el nombre"); 
+        nuevo.setNombre(lector.nextLine()); // Se guarda el nombre
+        System.out.println("Ingresa la curp"); 
+        nuevo.setCurp(lector.nextLine()); // Se guarda la curp
+        System.out.println("Ingresa la edad"); 
+        nuevo.setEdad(lector.nextInt()); // Se guarda la edad
         System.out.println("Ingresa el tipo de sangre");
-        nuevo.setTipoSangre(lector.next());
+        nuevo.setTipoSangre(lector.next()); // Se guarda el tipo de sangre
         System.out.print("\n");
-        nuevo.calcularPrioridad();
+        nuevo.calcularPrioridad(); // Se llama a la funcion calcularPrioridad()
         System.out.print("\n");
-        nuevo.sintomas();
+        nuevo.sintomas(); // Se llama a la funcion sintomas()
         System.out.print("\n");
-        return nuevo;
+        return nuevo; // Regresa el objeto de tipo paciente
     }
 
+    // Funcion para imprimir los pacientes
     public static void imprimir(PriorityQueue<paciente> cola) {
         System.out.println("\n---------------------------");
         for(paciente act : cola){
--- Hospital/src/paciente.java
+++ Hospital/src/paciente.java
@@ -1,62 +1,65 @@
 import java.util.Scanner;
 
 public class paciente {
-    private String nombre;
-    private String curp;
-    private int edad;
-    private String tipoSangre;
-    private String sintomas = "";
-    private Integer prioridad;
+    private String nombre; // Atributo para el nombre
+    private String curp; // Atributo para la curp
+    private int edad; // Atributo para la edad
+    private String tipoSangre; // Atributo para el tipoSangre
+    private String sintomas = ""; // Atributo para los sintomas
+    private Integer prioridad; // Atributo para la prioridad
 
-    public Integer getPrioridad() {
+    public Integer getPrioridad() { // Funcion get
         return prioridad;
     }
 
-    public void setNombre(String nombre) {
+    public void setNombre(String nombre) { // Funcion set
         this.nombre = nombre;
     }
 
-    public void setCurp(String curp) {
+    public void setCurp(String curp) { // Funcion set
         this.curp = curp;
     }
 
-    public void setEdad(int edad) {
+    public void setEdad(int edad) { // Funcion set
         this.edad = edad;
     }
 
-    public void setPrioridad(int prioridad) {
+    public void setPrioridad(int prioridad) { // Funcion set
         this.prioridad = prioridad;
     }
 
-    public void setTipoSangre(String tipoSangre) {
+    public void setTipoSangre(String tipoSangre) { // Funcion set
         this.tipoSangre = tipoSangre;
     }
 
-    public String getTipoSangre() {
+    public String getTipoSangre() { // Funcion get
         return tipoSangre;
     }
 
-    public String getNombre() {
+    public String getNombre() { // Funcion get
         return nombre;
     }
 
-    public String getSintomas() {
+    public String getSintomas() { // Funcion get
         return sintomas;
     }
 
-    public String getCurp() {
+    public String getCurp() { // Funcion get
         return curp;
     }
 
-    public int getEdad() {
+    public int getEdad() { // Funcion get
         return edad;
     }
 
-    public  paciente(){}
+    public  paciente(){} // Metodo constructor
 
+    // Funcion para calcular la prioridad de cada paciente
     public void calcularPrioridad(){
         Scanner lector = new Scanner(System.in);
         int opcion;
+        // El sistema de prioridad funciona por puntos, entre mas puntos, mas prioridad
+        // Un bebe tiene mas prioridad que un adulto, pero un anciano tiene mas que un bebe
         if(edad >= 0 && edad <=5){
             prioridad = 1;
         } else{
@@ -68,6 +71,11 @@ public class paciente {
             }
         }
         do{
+            // Toda la seccion siguiente es para determinar que pacientes necesitan atencion mas rapido
+            // basandose en el sistema de puntos de prioridad.
+            // Son 3 preguntas principales, ¿está conciente?, ¿tiene hemorragia? y ¿Tiene fracturas?
+            // Dentro de estas preguntas hay otras subpreguntas para hacer mas preciso el puntaje
+            // Cada pregunta está dentro de un do-while para poder repetir la pregunta en caso de que se ingrese una opcion no valida
             System.out.println("¿El paciente está conciente?");
             System.out.println("1) Si");
             System.out.println("2) No");
@@ -171,9 +179,11 @@ public class paciente {
         }while(opcion != 1 && opcion != 2);    
     }
     
+    // Funcion para agregar sintomas al registro
     public void sintomas(){
         Scanner lector = new Scanner(System.in);
         String opcion, personalizados;
+        // Se muestra una lista de sintomas predefinidos, con la posibilidad de agregar otros que el usuario quiera
         System.out.println("--Escoge los sintomas--");
         System.out.println("1) Dolor de cabeza");
         System.out.println("2) Fiebre");
@@ -183,8 +193,9 @@ public class paciente {
         System.out.println("6) Diarrea");
         System.out.println("7) Dolor de garganta");
         System.out.println("8) Agregar algunos");
-        opcion = lector.nextLine();
-        String[] sintomas2 = new String[7];
+        System.out.println("*Puedes escoger varias opciones separandolas con coma (ejemplo: 1,2,3)");
+        opcion = lector.nextLine(); // Se guardan las opciones seleccionadas en la variable opcion
+        String[] sintomas2 = new String[7]; // Se guardan todas opciones seleccionadas en arreglo de tipo string
         sintomas2[0] = "Dolor de cabeza";
         sintomas2[1] = "Fiebre";
         sintomas2[2] = "Dolor en una zona en especifico";
@@ -192,10 +203,12 @@ public class paciente {
         sintomas2[4] = "Vomito";
         sintomas2[5] = "Diarrea";
         sintomas2[6] = "Dolor de garganta";
-        opcion = opcion.replaceAll("\s+", "");
-        opcion = opcion.replaceAll(",", "");
-        char[] letras = opcion.toCharArray();
-        for(int i = 0; i < letras.length; i++){
+        opcion = opcion.replaceAll("\s+", ""); // Se eliminan espacios en opcion
+        opcion = opcion.replaceAll(",", ""); // Se eliminan las comas en opcion
+        char[] letras = opcion.toCharArray(); // Se convierte la variable opcion en un arreglo de caracteres
+        // Se analiza cada caracter y se buscan coincidencias con las opciones disponibles
+        // Cuando se encuentran se concatena la variable sintomas con la posicion correspondiente del arreglo sintomas
+        for(int i = 0; i < letras.length; i++){ 
             if(i == 0){
                 if(letras[i] == '1'){
                 sintomas = sintomas2[0]; 
@@ -218,12 +231,14 @@ public class paciente {
                 if(letras[i] == '7'){
                     sintomas = sintomas2[6]; 
                 }
-                if(letras[i] == '8'){
-                    System.out.println("Ingrese sus sintomas");
-                    personalizados = lector.nextLine();
-                    sintomas = sintomas + personalizados;
+                if(letras[i] == '8'){ // Cuando es 8 es para agregar sintomas personalizados
+                    System.out.println("Ingrese sus sintomas"); // Se imprime un mensaje
+                    personalizados = lector.nextLine(); // Se leen los sintomas extras 
+                    sintomas = sintomas + personalizados; // Se concatenan con los que ya existian
                 }
             } else {
+                // Esta segunda lista de if es para cuando ya existian sintomas en la variable sintomas.
+                // Con el simple objetivo de agregar las comas para separar los sintomas
                 if(letras[i] == '1'){
                 sintomas = sintomas + ", " + sintomas2[0]; 
                 }
@@ -248,7 +263,7 @@ public class paciente {
                 if(letras[i] == '8'){
                     System.out.println("Ingrese sus sintomas");
                     personalizados = lector.nextLine();
-                    sintomas = sintomas + ". " + personalizados;
+                    sintomas = sintomas + ", " + personalizados;
                 }
             }
         }
